4e8c56fbade7bbe281b08ae5275c6cc71c93d0d7
Sean Pollard <seanypollard@Seans-MBP.lan>
2020-10-07 16:28:01 +0100

    initial add

---
+++ week-1/sample_2.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const server = http.createServer();
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017";
+const dbName = "student";
+const collectionName = "students";
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+server.on("request", async (req, res) => {
+  const { url, headers } = req;
+  try {
+    const students = [
+      {
+        name: { first: "joe", last: "appleton" },
+        dob: new Date("August 12, 1982"),
+      },
+      {
+        name: { first: "bill", last: "smith" },
+        dob: new Date("August 12, 1982"),
+      },
+    ];
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    await collection.insertMany(students);
+    console.log("connected");
+    res.end("request ended");
+  } catch (e) {
+    console.log(e);
+    res.end("could not update");
+  }
+});
+
+server.listen(8080);
